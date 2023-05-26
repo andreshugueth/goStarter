20226ab44d22ca7492afc06c96a047c4413d7707
Andres Hugueth <andreshugueth@gmail.com>
2023-05-26 15:44:24 -0500

    05 area and reader implementing interfaces

---
+++ 05-area/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+type square struct {
+	sideLenght float64
+}
+
+func main() {
+	t := triangle{
+		base:   2.0,
+		height: 3.5,
+	}
+	s := square{
+		sideLenght: 3.0,
+	}
+	printArea(t)
+	printArea(s)
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func (s square) getArea() float64 {
+	return s.sideLenght * s.sideLenght
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
